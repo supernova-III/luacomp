2f8a358a233f57d0558330795e3ed428cc19ded8
supernova-III <belashkiril@gmail.com>
2023-07-22 10:31:05 +0200

    Internal tokenizer interface defined

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,37 +1,149 @@
 #include <string.h>
 #include "tokenizer.h"
 #include "common.h"
+#include "tokenizer_internal.h"
 
-static inline bool isAlpha(char c) {
-  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-}
+typedef struct TokenIterator {
+  const char *input;
+  size_t len;
+  const char *it;
+  Token current;
+} TokenIterator;
 
-static inline int64_t hexCharToNumber(char c) {
-  // convert character to upper case
-  if (c >= 'a') {
-    c = c - ('A' - 'a');
+static TokenIterator iterator = {.current.type = TOKEN_INVALID};
+static StringTable *string_table = NULL;
+static MemoryPool *string_nodes_pool = NULL;
+static MemoryPool *identifier_memory_pool = NULL;
+
+void InitTokenizer(const char *input, size_t len) {
+  iterator.input = input;
+  iterator.len = len;
+  iterator.it = input;
+  string_table = allocateStringTable(128 * sizeof(StringList));
+  string_nodes_pool = allocateMemoryPool(128 * 6 * sizeof(StringNode));
+  identifier_memory_pool = allocateMemoryPool(32 * 1024 * 1024);
+  if (!string_table || !string_nodes_pool || !identifier_memory_pool) {
+    printf("No enough memory\n");
   }
-  return c - 'A' + 10;
 }
 
-static inline bool isDigit(char c) {
-  return c >= '0' && c <= '9';
+const Token *PeekToken() {
+  return &iterator.current;
 }
 
-static inline bool isHexadecimal(char c) {
-  return isDigit(c) || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+const Token *NextToken() {
+  char c = peekCharacter();
+
+repeat:
+  if (!c) {
+    iterator.current.type = TOKEN_END_OF_STREAM;
+    return &iterator.current;
+  }
+
+  switch (c) {
+    case '"': {
+    } break;
+    case '\'': {
+    } break;
+    case '-': {
+      c = getNextCharacter();
+      if (c != '-') {
+        iterator.current.type = TOKEN_MINUS;
+      } else {
+        while (c != '\n' && c != 0) {
+          c = getNextCharacter();
+        }
+        goto repeat;
+      }
+    } break;
+    case '.': {
+      iterator.current.type = TOKEN_PERIOD;
+      c = getNextCharacter();
+      if (c == '.') {
+        iterator.current.type = TOKEN_2PERIOD;
+        c = getNextCharacter();
+        if (c == '.') {
+          iterator.current.type = TOKEN_3PERIOD;
+          advanceInputIterator();
+        }
+      } else if (isDigit(c)) {
+        // here we parse numbers like .123 or .123e123
+        c = getNextCharacter();
+        while (isDigit(c)) {
+          c = getNextCharacter();
+        }
+
+        if (c == 'e' || c == 'E') {
+          // exponent
+        } else {
+        }
+      }
+    } break;
+    // clang-format off
+    case '0': case '1': case '2': case '3': case '4': case '5':
+    case '6': case '7': case '8': case '9': {
+      // clang-format on
+      // There are different kinds of numbers in lua:
+      // 1. Decimal integers: [0-9]+
+      // 2. Hex integers: 0x[0-9a-fA-F]+
+      // 3. Float numbers: [0-9]+.[0-9]* ([eE][+-][0-9]+)?
+      // 5. Hexadecimal float: 0x([0-9a-fA-F]*.[0-9a-fA-F]+ |
+      // [0-9a-fA-F]+.[0-9a-fA-F]*)([pP][+-][0-9]+)?
+    } break;
+    // clang-format off
+    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
+    case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+    case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+    case 'v': case 'w': case 'x': case 'y': case 'z': case 'A': case 'B':
+    case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':
+    case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P':
+    case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W':
+    case 'X': case 'Y': case 'Z': {
+      // clang-format on
+      const char *start = iterator.it;
+      c = getNextCharacter();
+      while (isAlpha(c) || isDigit(c) || c == '_') {
+        c = getNextCharacter();
+      }
+      const size_t len = iterator.it - start;
+      TokenType token_type = lookupKeyword(start, len);
+      if (token_type != TOKEN_INVALID) {
+        iterator.current.type = token_type;
+      } else {
+        iterator.current.type = TOKEN_IDENTIFIER;
+        Identifier *identifier = insertString(&string_table, start, len);
+        if (!identifier) {
+          printf("Cannot store identifier\n");
+        }
+        iterator.current.value.identifier = identifier;
+      }
+    } break;
+    case ' ':
+    case '\t':
+    case '\n': {
+      c = getNextCharacter();
+      goto repeat;
+    } break;
+    // clang-format off
+    case '+': case '*': case '%': case '#': case '&': case '|': case '(':
+    case ')': case '{': case '}': case '[': case ']': case ';': case ',': {
+      // clang-format on
+      iterator.current.type = c;
+      advanceInputIterator();
+    } break;
+    // clang-format off
+    case '/': case '~': case '<': case '>': case '=': case ':': {
+      // clang-format on
+      iterator.current.type = scanWithTable(c);
+    } break;
+    default: {
+      printf("Unexpected character: %c\n", c);
+    }
+  }
+  return &iterator.current;
 }
 
-static inline bool isBinary(char c) {
-  return c == '0' || c == '1';
-}
-
-static inline int64_t charToDigit(char c) {
-  return c - '0';
-}
-
-static inline unsigned int hashKeyword(
-    register const char *str, register size_t len) {
+static inline uint32_t hashKeyword(const char *str, size_t len) {
   static unsigned char asso_values[] = {30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
       30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
       30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,
@@ -51,21 +163,12 @@ static inline unsigned int hashKeyword(
          asso_values[(unsigned char)str[0]];
 }
 
-typedef struct {
-  const char *string;
-  size_t len;
-  TokenType token_type;
-} HashTableEntry;
-
-#define HASH_TABLE_ENTRY(literal, type) \
-  { .string = literal, .len = sizeof(literal) - 1, .token_type = type }
-
 #define TOTAL_KEYWORDS 22
 #define MIN_WORD_LENGTH 2
 #define MAX_WORD_LENGTH 8
 #define MIN_HASH_VALUE 2
 #define MAX_HASH_VALUE 29
-static TokenType lookupKeyword(register const char *str, register size_t len) {
+static TokenType lookupKeyword(const char *str, size_t len) {
   // clang-format off
   static HashTableEntry wordlist[] = {
     {.string = "", .len = 0, .token_type = TOKEN_INVALID}, 
@@ -113,26 +216,6 @@ static TokenType lookupKeyword(register const char *str, register size_t len) {
   return TOKEN_INVALID;
 }
 
-// seed may be 5381
-// seed may be 5387
-static inline uint32_t hashString(const char *str, size_t len, uint32_t seed) {
-  uint32_t h = seed ^ (uint32_t)len;
-  for (; len > 0; len--) {
-    h ^= ((h << 5) + (h >> 2) + (uint8_t)str[len - 1]);
-  }
-  return h;
-}
-
-typedef struct {
-  size_t size;
-  size_t capacity;
-  uint8_t memory[];
-} MemoryPool;
-
-static inline size_t computeMemoryPoolSpace(size_t capacity) {
-  return sizeof(MemoryPool) + capacity;
-}
-
 static MemoryPool *allocateMemoryPool(size_t capacity) {
   MemoryPool *pool = (MemoryPool *)Calloc(1, computeMemoryPoolSpace(capacity));
   if (pool) {
@@ -157,10 +240,6 @@ static uint8_t *allocateFromMemoryPool(MemoryPool **pool, size_t n) {
   return memory;
 }
 
-typedef StringView Identifier;
-
-static MemoryPool *identifier_memory_pool = NULL;
-
 static Identifier *allocateIdentifier(const char *str, size_t len) {
   Identifier *res = (Identifier *)allocateFromMemoryPool(
       &identifier_memory_pool, len + 1 + sizeof(Identifier));
@@ -171,32 +250,11 @@ static Identifier *allocateIdentifier(const char *str, size_t len) {
   return res;
 }
 
-typedef struct StringNode {
-  struct StringNode *next;
-  Identifier *identifier;
-} StringNode;
-
-typedef struct {
-  StringNode *head;
-  StringNode *tail;
-} StringList;
-
-static MemoryPool *string_nodes_pool = NULL;
-
 static StringNode *allocateStringNode() {
   StringNode *node = (StringNode *)allocateFromMemoryPool(
       &string_nodes_pool, sizeof(StringNode));
   return node;
 }
-
-typedef struct {
-  double max_load;
-  size_t hash_seed;
-  size_t item_size;
-  size_t capacity;
-  size_t n_buckets;
-  StringList buckets[];
-} StringTable;
 
 static StringTable *allocateStringTable(size_t capacity) {
   const size_t item_size = sizeof(StringList);
@@ -210,11 +268,6 @@ static StringTable *allocateStringTable(size_t capacity) {
   }
   return table;
 }
-
-typedef struct {
-  bool collision;
-  StringNode *node;
-} InsertStringResult;
 
 static InsertStringResult insertStringBasic(
     StringTable *table, const char *str, size_t len) {
@@ -311,28 +364,6 @@ static const char *lookupStringTable(
   return result;
 }
 
-typedef struct TokenIterator {
-  const char *input;
-  size_t len;
-  const char *it;
-  Token current;
-} TokenIterator;
-
-static TokenIterator iterator = {.current.type = TOKEN_INVALID};
-static StringTable *string_table = NULL;
-
-void InitTokenizer(const char *input, size_t len) {
-  iterator.input = input;
-  iterator.len = len;
-  iterator.it = input;
-  string_table = allocateStringTable(128 * sizeof(StringList));
-  string_nodes_pool = allocateMemoryPool(128 * 6 * sizeof(StringNode));
-  identifier_memory_pool = allocateMemoryPool(32 * 1024 * 1024);
-  if (!string_table || !string_nodes_pool || !identifier_memory_pool) {
-    printf("No enough memory\n");
-  }
-}
-
 static inline void advanceInputIterator() {
   if (iterator.it - iterator.input < iterator.len) {
     ++iterator.it;
@@ -346,10 +377,6 @@ static inline char getNextCharacter() {
 
 static inline char peekCharacter() {
   return *iterator.it;
-}
-
-const Token *PeekToken() {
-  return &iterator.current;
 }
 
 typedef struct {
@@ -424,101 +451,4 @@ static inline TokenType scanWithTable(char c) {
     res = entry->pairs[i].t;
   }
   return res;
-}
-
-const Token *NextToken() {
-  char c = peekCharacter();
-
-repeat:
-  if (!c) {
-    iterator.current.type = TOKEN_END_OF_STREAM;
-    return &iterator.current;
-  }
-
-  switch (c) {
-    case '"': {
-    } break;
-    case '\'': {
-    } break;
-    case '-': {
-      c = getNextCharacter();
-      if (c != '-') {
-        iterator.current.type = TOKEN_MINUS;
-      } else {
-        while (c != '\n' && c != 0) {
-          c = getNextCharacter();
-        }
-        goto repeat;
-      }
-    } break;
-    case '.': {
-      iterator.current.type = TOKEN_PERIOD;
-      c = getNextCharacter();
-      if (c == '.') {
-        iterator.current.type = TOKEN_2PERIOD;
-        c = getNextCharacter();
-        if (c == '.') {
-          iterator.current.type = TOKEN_3PERIOD;
-          advanceInputIterator();
-        }
-      }
-    } break;
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9': {
-    } break;
-    // clang-format off
-    case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
-    case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
-    case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
-    case 'v': case 'w': case 'x': case 'y': case 'z': case 'A': case 'B':
-    case 'C': case 'D': case 'E': case 'F': case 'G': case 'H': case 'I':
-    case 'J': case 'K': case 'L': case 'M': case 'N': case 'O': case 'P':
-    case 'Q': case 'R': case 'S': case 'T': case 'U': case 'V': case 'W':
-    case 'X': case 'Y': case 'Z': {
-      // clang-format on
-      const char *start = iterator.it;
-      c = getNextCharacter();
-      while (isAlpha(c) || isDigit(c) || c == '_') {
-        c = getNextCharacter();
-      }
-      const size_t len = iterator.it - start;
-      TokenType token_type = lookupKeyword(start, len);
-      if (token_type != TOKEN_INVALID) {
-        iterator.current.type = token_type;
-      } else {
-        iterator.current.type = TOKEN_IDENTIFIER;
-        Identifier *identifier = insertString(&string_table, start, len);
-        if (!identifier) {
-          printf("Cannot store identifier\n");
-        }
-        iterator.current.value.identifier = identifier;
-      }
-    } break;
-    case ' ':
-    case '\t':
-    case '\n': {
-      c = getNextCharacter();
-      goto repeat;
-    } break;
-    // clang-format off
-    case '+': case '*': case '%': case '#': case '&': case '|': case '(':
-    case ')': case '{': case '}': case '[': case ']': case ';': case ',': {
-      // clang-format on
-      iterator.current.type = c;
-      advanceInputIterator();
-    } break;
-
-    default: {
-      iterator.current.type = scanWithTable(c);
-    }
-  }
-  return &iterator.current;
 }
