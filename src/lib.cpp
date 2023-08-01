4ae13d553027eff92ac067861c3b8de396ac9b0f
supernova-III <belashkiril@gmail.com>
2023-08-01 21:08:08 +0200

    Moved all implementations from lib.hh to lib.cpp

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,10 +1,31 @@
 #include "lib.hh"
 #include <cstdlib>
 
+#define FORMAT_MESSAGE(format, what, szvar)                 \
+  va_list args;                                             \
+  va_start(args, format);                                   \
+  auto szvar = vsnprintf(what, sizeof(what), format, args); \
+  va_end(args);
+
+RuntimeError::RuntimeError(const char* format, ...) {
+  FORMAT_MESSAGE(format, what_, _);
+}
+
+SystemError::SystemError(const char* format, ...) {
+  static char what[256] = {};
+  FORMAT_MESSAGE(format, what, size);
+  const char* first = what;
+  char* second = what + size + 1;
+  strerror_s(second, sizeof(what) - size - 1, errno);
+  snprintf(what_, sizeof(what_), "%s: %s\n", first, second);
+}
+
 PoolAllocator::PoolAllocator(size_t size)
     : current_pool_(nullptr), n_pools_(0) {
   allocatePool(size);
 }
+
+#undef FORMAT_MESSAGE
 
 uint8_t* PoolAllocator::Allocate(size_t size) {
   const auto new_size = size + current_pool_->size;
--- src/lib.hh
+++ src/lib.hh
@@ -6,12 +6,6 @@
 #include <cstdio>
 #include <cstdarg>
 
-#define FORMAT_MESSAGE(format, what, szvar)                 \
-  va_list args;                                             \
-  va_start(args, format);                                   \
-  auto szvar = vsnprintf(what, sizeof(what), format, args); \
-  va_end(args);
-
 class RuntimeError {
  protected:
   char what_[256] = {};
@@ -19,7 +13,7 @@ class RuntimeError {
   RuntimeError() = default;
 
  public:
-  RuntimeError(const char* format, ...) { FORMAT_MESSAGE(format, what_, _); }
+  RuntimeError(const char* format, ...);
 
   const char* What() const { return what_; }
   void PrintToStdOut() const { std::printf("%s\n", what_); }
@@ -27,17 +21,8 @@ class RuntimeError {
 
 class SystemError final : public RuntimeError {
  public:
-  SystemError(const char* format, ...) {
-    static char what[256] = {};
-    FORMAT_MESSAGE(format, what, size);
-    const char* first = what;
-    char* second = what + size + 1;
-    strerror_s(second, sizeof(what) - size - 1, errno);
-    snprintf(what_, sizeof(what_), "%s: %s\n", first, second);
-  }
+  SystemError(const char* format, ...);
 };
-
-#undef FORMAT_MESSAGE
 
 // Allocator that allocates memory from buffers that connected via linked
 // list, to avoid expensive reallocation when another buffer is full. This
