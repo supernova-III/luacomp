8515855901ba58fb16b6fd1a0368ed6e883ccf54
supernova-III <belashkiril@gmail.com>
2023-08-21 19:41:12 +0200

    Removed STL stuff to make build faster

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,57 @@
-#include <fstream>
-#include <iterator>
-#include <vector>
+#include <cstdio>
+#include <cstdlib>
 #include "lib.hh"
 #include "tokenizer.hh"
 
+struct File {
+  char* buffer;
+  size_t size;
+  FILE* handle;
+
+  File(const char* path) {
+    auto error = fopen_s(&handle, path, "rb");
+    if (error) {
+      throw SystemError("Unable to create file %s", path);
+    }
+    if (fseek(handle, 0, SEEK_END)) {
+      throw SystemError("Unable to get input file size.");
+    }
+    const auto file_size = ftell(handle);
+    fseek(handle, 0, 0);
+    buffer = static_cast<char*>(calloc(file_size + 1, 1));
+    if (!buffer) {
+      throw RuntimeError("Unable to allocate memory to read input file.");
+    }
+    size = fread(buffer, 1, file_size, handle);
+    if (size != file_size) {
+      throw SystemError("Unable to read input file %s", path);
+    }
+  }
+
+  File() = delete;
+  File(const File&) = delete;
+  File& operator=(const File&) = delete;
+  File(File&&) = default;
+  File& operator=(File&&) = default;
+
+  String Content() const { return String(buffer, size); }
+
+  ~File() {
+    if (buffer) {
+      free(buffer);
+    }
+    if (handle) {
+      fclose(handle);
+    }
+  }
+
 int main(int argc, char* argv[]) try {
-  if (argc != 2) {
+  if (argc < 2) {
     throw RuntimeError("Usage: %s <source_file>", argv[0]);
   }
 
-  auto file = std::ifstream(argv[1], std::ios::in);
-  if (!file) {
-    throw SystemError("Unable to create file %s", argv[1]);
-  }
-  const auto data = std::vector<char>(std::istreambuf_iterator<char>(file), {});
-
-  auto iter = TokenIterator(argv[1], String(data.data(), data.size()));
+  auto file = File(argv[1]);
+  auto iter = TokenIterator(argv[1], file.Content());
   while (++iter) {
     auto current = *iter;
   }
